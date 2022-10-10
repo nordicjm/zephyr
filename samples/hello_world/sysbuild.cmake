bb384fe2d923c65d685410d7115b3bd0a77308d0
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2022-10-10 15:59:19 +0100

    Test files

---
+++ samples/hello_world/sysbuild.cmake
@@ -0,0 +1,9 @@
+  ExternalZephyrProject_Add(
+    APPLICATION mcuboot
+    SOURCE_DIR ${ZEPHYR_MCUBOOT_MODULE_DIR}/boot/zephyr/
+SYSBUILD_MODULE_DIRS ${SYSBUILD_MODULE_DIRS}
+SYSBUILD_CMAKE_DIRS ${SYSBUILD_CMAKE_DIRS}
+  )
+  # MCUBoot default configuration is to perform a full chip erase.
+  # Placing MCUBoot first in list to ensure it is flashed before other images.
+  set(IMAGES "mcuboot" ${IMAGES} PARENT_SCOPE)
