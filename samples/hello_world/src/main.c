10f3d87750cdf8ce69aad44555332a2cd657b5ed
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2023-08-22 14:41:01 +0100

    Add test application

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,28 @@
 
 #include <zephyr/kernel.h>
 
+#ifdef CONFIG_RETENTION
+#include <zephyr/retention/retention.h>
+#include <zephyr/retention/blinfo.h>
+#endif
+
 int main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+
+#ifdef CONFIG_RETENTION
+	uint8_t retval;
+	int rc;
+
+	rc = blinfo_lookup(BLINFO_MODE, &retval, sizeof(retval));
+	printk("Mode (%d): %d\n", rc, retval);
+
+	rc = blinfo_lookup(BLINFO_SIGNATURE_TYPE, &retval, sizeof(retval));
+	printk("Signature type (%d): %d\n", rc, retval);
+
+	rc = blinfo_lookup(BLINFO_RUNNING_SLOT, &retval, sizeof(retval));
+	printk("Slot (%d): %d\n", rc, retval);
+#endif
+
 	return 0;
 }
