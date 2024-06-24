653e24de0476f3c4aebcb6a44b7790b17a18e3b3
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-06-24 17:11:24 +0100

    IR test

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -20,6 +20,358 @@
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
+void trigger_signal()
+{
+//uint32_t loops = 17615;
+uint32_t loops = 15;
+int ret;
+
+while (loops > 0) {
+		ret = gpio_pin_toggle_dt(&led);
+//tmp = sys_clock_cycle_get_32() + 1;
+//while (tmp > sys_clock_cycle_get_32());
+
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+
+//77 = 7.3us
+//1 = 0.0948051948052
+//8.564
+
+#if 0
+tmp = 12;
+while (tmp > 0)
+{
+--tmp;
+}
+#endif
+
+		ret = gpio_pin_toggle_dt(&led);
+
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+
+--loops;
+}
+}
+
 int main(void)
 {
 	int ret;
@@ -29,20 +381,83 @@ int main(void)
 		return 0;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
 	if (ret < 0) {
 		return 0;
 	}
 
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
+uint8_t data[] = {
+0b01001010,
+0b01110101,
+0b11000011,
+0b01100100,
+0b10011011,
+0b11111111,
+0b00000000,
+0b11110110,
+0b00001001,
+0b01100111,
+0b10011000
+};
 
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
-		k_msleep(SLEEP_TIME_MS);
-	}
+trigger_signal();
+trigger_signal();
+trigger_signal();
+trigger_signal();
+trigger_signal();
+trigger_signal();
+trigger_signal();
+trigger_signal();
+
+//k_busy_wait(1606);
+k_busy_wait(1608);
+__asm("NOP");
+
+//k_sleep(K_MSEC(40));
+
+uint8_t q = 0;
+while (q < ARRAY_SIZE(data))
+{
+uint8_t check = data[q];
+uint8_t t = 0;
+while (t < 8)
+{
+
+trigger_signal();
+
+if ((check & 0x80) == 0)
+{
+//420us
+//k_busy_wait(420);
+k_busy_wait(404);
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+__asm("NOP");
+
+}
+else
+{
+//1.214ms
+k_busy_wait(1205);
+__asm("NOP");
+}
+
+check = check << 1;
+++t;
+}
+++q;
+}
+
+trigger_signal();
+
+
+//		ret = gpio_pin_toggle_dt(&led);
 	return 0;
 }
