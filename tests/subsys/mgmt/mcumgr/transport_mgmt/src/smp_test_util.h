38dd2dcf35fc985a9126f8a3c2869521ce5acc38
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2026-02-10 16:46:13 +0000

    Add initial transport mgmt shell of test

---
+++ tests/subsys/mgmt/mcumgr/transport_mgmt/src/smp_test_util.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef H_SMP_TEST_UTIL_
+#define H_SMP_TEST_UTIL_
+
+#include <zephyr/ztest.h>
+#include <zephyr/mgmt/mcumgr/mgmt/mgmt.h>
+#include <zcbor_common.h>
+#include <smp_internal.h>
+
+/* Function for creating an os_mgmt echo command */
+bool create_os_mgmt_echo_packet(zcbor_state_t *zse, uint8_t *buffer, uint8_t *output_buffer,
+				uint16_t *buffer_size, char *data);
+
+/* Function for creating a transport_mgmt connect command */
+bool create_transport_mgmt_connect_packet(zcbor_state_t *zse, uint8_t *buffer,
+					  uint8_t *output_buffer, uint16_t *buffer_size,
+					  uint8_t transport_id);
+
+/* Function for creating a transport_mgmt disconnect command */
+bool create_transport_mgmt_disconnect_packet(zcbor_state_t *zse, uint8_t *buffer,
+					     uint8_t *output_buffer, uint16_t *buffer_size,
+					     uint8_t transport_id, bool all);
+
+/* Function for creating a transport_mgmt status command */
+bool create_transport_mgmt_status_packet(zcbor_state_t *zse, uint8_t *buffer,
+					 uint8_t *output_buffer, uint16_t *buffer_size);
+
+#endif
